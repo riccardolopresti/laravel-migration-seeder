e941c935f46b178483c64c9ac2f9efd07bf6ab10
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-10 13:46:11 +0100

    init

---
+++ database/migrations/2023_01_10_124449_create_trains_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            /*
+                Azienda
+                Stazione di partenza
+                Stazione di arrivo
+                Orario di partenza
+                Orario di arrivo
+                Codice Treno
+                Numero Carrozze
+                In orario
+                Cancellato
+            */
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
